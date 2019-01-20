3510dfcd4719339fbd784bba0c561969d0505f87
Denny Zhang <denny.zhang001@gmail.com>
2019-01-20 10:25:29 -0800

    jq double quote

--- cleanup-bosh-deployment.sh
+++ cleanup-bosh-deployment.sh
@@ -5,14 +5,14 @@
 ## Link: https://cheatsheet.dennyzhang.com/cheatsheet-bosh-A4
 ## --
 ## Created : <2019-01-18>
-## Updated: Time-stamp: <2019-01-20 10:20:59>
+## Updated: Time-stamp: <2019-01-20 10:25:18>
 ##-------------------------------------------------------------------
 # shellcheck disable=SC1091
 source /home/kubo/gw_scripts/bosh_env.sh
 
 if [ -z "$BOSH_DEPLOYMENT_DELETE" ]; then
     echo "BOSH_DEPLOYMENT_DELETE env is empty. Deleting all bosh deployments"
-    for d in $(bosh deployments --json | jq .Tables[].Rows[].name); do
+    for d in $(bosh deployments --json | jq -r .Tables[].Rows[].name); do
         echo "echo y | bosh delete-deployment --force -d $d"
         echo y | bosh delete-deployment --force -d "$d"
     done
--- cleanup-bosh-release.sh
+++ cleanup-bosh-release.sh
@@ -5,14 +5,14 @@
 ## Link: https://cheatsheet.dennyzhang.com/cheatsheet-bosh-A4
 ## --
 ## Created : <2019-01-18>
-## Updated: Time-stamp: <2019-01-20 10:20:50>
+## Updated: Time-stamp: <2019-01-20 10:25:11>
 ##-------------------------------------------------------------------
 # shellcheck disable=SC1091
 source /home/kubo/gw_scripts/bosh_env.sh
 
 if [ -z "$BOSH_RELEASE_DELETE" ]; then
     echo "BOSH_RELEASE_DELETE env is empty. Deleting all bosh releases"
-    for b in $(bosh releases --json | jq .Tables[].Rows[].name); do
+    for b in $(bosh releases --json | jq -r .Tables[].Rows[].name); do
         echo "echo y | bosh delete-release $b"
         echo y | bosh delete-release "$b"
     done
