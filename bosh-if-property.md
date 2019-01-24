ec39190351a3c62d25fd5c20106457d775cc380c
Denny Zhang <denny.zhang001@gmail.com>
2019-01-23 23:20:19 -0800

    update pdf

---
+++ bosh-if-property.md
@@ -0,0 +1,7 @@
+# https://ultimateguidetobosh.com/properties/
+
+```
+<% if_p('some-property') do |value| %>
+optional-config-item: <%= value %>
+<% end %>
+```
--- cleanup-bosh-release.sh
+++ cleanup-bosh-release.sh
@@ -5,15 +5,15 @@
 ## Link: https://cheatsheet.dennyzhang.com/cheatsheet-bosh-A4
 ## --
 ## Created : <2019-01-18>
-## Updated: Time-stamp: <2019-01-20 11:25:54>
+## Updated: Time-stamp: <2019-01-20 22:16:36>
 ## Usage:
 ## # ssh
 ## ssh -o StrictHostKeyChecking=no root@10.92.171.234 \
-##     "curl -L https://raw.githubusercontent.com/dennyzhang/cheatsheet-bosh-A4/master/cleanup-bosh-release.sh | bash"
+##  "curl -L https://raw.githubusercontent.com/dennyzhang/cheatsheet-bosh-A4/master/cleanup-bosh-release.sh | bash"
 ##
 ## # ssh without password, and delete a given bosh release
 ## sshpass -p 'MY_SSH_PASSWORD' ssh -o StrictHostKeyChecking=no root@10.92.171.234 \
-##     "export BOSH_RELEASE_DELETE=kubo; curl -L https://raw.githubusercontent.com/dennyzhang/cheatsheet-bosh-A4/master/cleanup-bosh-release.sh | bash"
+##  "export BOSH_RELEASE_DELETE=kubo; curl -L https://raw.githubusercontent.com/dennyzhang/cheatsheet-bosh-A4/master/cleanup-bosh-release.sh | bash"
 ##-------------------------------------------------------------------
 # shellcheck disable=SC1091
 source /home/kubo/gw_scripts/bosh_env.sh
